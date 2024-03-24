5c7465795d830761d887bd4d4a57c0cf2505a269
coka13 <isafeze@gmail.com>
2024-03-24 19:39:51 +0200

    Created skeleton for homepage

---
+++ WorkSwipe/src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CustomDrawer } from '../../components/CustomDrawer/CustomDrawer'
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import JoinInnerIcon from '@mui/icons-material/JoinInner';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import InfoIcon from '@mui/icons-material/Info';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const Homepage = () => {
+    const icons = [AccountBoxIcon,JoinInnerIcon,ContactSupportIcon,InfoIcon,LogoutIcon]
+  return (
+    <>
+    <div>Homepage</div>
+    <CustomDrawer items={['Profile', 'Matches','Support','About Us', 'Logout']} icons = {icons}/>
+    </>
+  )
+}
+
+export default Homepage
