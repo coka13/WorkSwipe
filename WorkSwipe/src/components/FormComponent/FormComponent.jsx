d237bbde8b59599d0009cdf65fdfa2718b48a807
coka13 <isafeze@gmail.com>
2024-04-01 21:09:31 +0300

    added checkbox to the register and fixed styling

--- WorkSwipe/src/components/FormComponent/FormComponent.jsx
+++ WorkSwipe/src/components/FormComponent/FormComponent.jsx
@@ -1,31 +1,41 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import SelectVariants from "../SelectComponent/SelectComponent";
+import "./FormComponent.css";
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 
 export default function FormComponent({ props }) {
-  
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
+    <Box component="form" noValidate autoComplete="off">
       <div className="FieldsWraper">
         {props.map((prop) => {
-          if(!prop.id){
-            prop.id= 'outlined-basic'
+          if (!prop.id) {
+            prop.id = "outlined-basic";
           }
           if (prop.type === "select") {
-
             return <SelectVariants prop={prop} />;
+          } else if (prop.type === "check") {
+            return (
+              <div
+                className="scrollable-container"
+              >
+                <FormGroup>
+                  {prop.options.map((option) => (
+                    <FormControlLabel
+                      key={option}
+                      label={option}
+                      control={<Checkbox />}
+                    />
+                  ))}
+                </FormGroup>
+              </div>
+            );
           } else {
             return (
               <TextField
+                key={prop.id}
                 required={prop.required}
-                variant= {prop.variant}
+                variant={prop.variant}
                 id={prop.id}
                 label={prop.name}
                 type={prop.type}
