eb34cfd87edb39e0eb10817966d49be1b9e155cf
asaf karaso <asafkaraso@asafs-MacBook-Pro.local>
2024-04-01 19:10:13 +0300

    registerPage name updated

--- WorkSwipe/src/App.jsx
+++ WorkSwipe/src/App.jsx
@@ -9,7 +9,7 @@ import ContactSupportIcon from "@mui/icons-material/ContactSupport";
 import InfoIcon from "@mui/icons-material/Info";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 
@@ -39,7 +39,7 @@ function App() {
         />
       )}
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<Homepage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<Login />} />
