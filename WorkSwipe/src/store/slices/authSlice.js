785ea4cb4d26665cd2727941ab55fbe4c862998f
asaf karaso <asafkaraso@asafs-MacBook-Pro.local>
2024-05-23 18:58:01 +0300

    admin,jobSeeker,employer,auth slices

---
+++ WorkSwipe/src/store/slices/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    isAuthenticated: false,
+    _id:"",
+    role:""
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuthentication:(state,action)=>{
+      state.isAuthenticated=action.payload
+    }
+  }
+});
+
+export const { setAuthentication} = authSlice.actions
+
+export default authSlice.reducer
