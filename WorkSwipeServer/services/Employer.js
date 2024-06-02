8640f9c929cc310c74a1c6f74eecc4ac834d31e4
Asaf Karaso <asaf.karaso35@gmail.com>
2024-06-02 23:59:54 +0300

    username in service

--- WorkSwipeServer/services/Employer.js
+++ WorkSwipeServer/services/Employer.js
@@ -10,4 +10,4 @@ export const createEmployerService = (form) => new Employer(form)
 
 export const deleteEmployerService=(id)=>Employer.findOneAndDelete({ _id: id})
 
-export const getSingleEmployerByNameService = (name) => Employer.findOne({ name: name })
+export const getSingleEmployerByNameService = (username) => Employer.findOne({ username: username })
